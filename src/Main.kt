cae6d74bbcaf17276adaee3a34ab45201f530129
diemaf2004 <you@example.com>
2024-06-13 11:39:49 -0500

    primer cambio

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//fun main() {
+//    val favoriteActor = null
+//    println(favoriteActor)
+//}
+//fun main() {
+//    var favoriteActor: String = "Sandra Oh"
+//    favoriteActor = null
+//}
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//    println(favoriteActor)
+//
+//    favoriteActor = null
+//    println(favoriteActor)
+//}
---
+++ src/main 2.kt
@@ -0,0 +1,38 @@
+//fun main() {
+//    var number: Int? = 10
+//    println(number)
+//}
+//fun main() {
+//    var number: Int? = 10
+//    println(number)
+//
+//    number = null
+//    println(number)
+//}
+//fun main() {
+//    var favoriteActor: String = "Sandra Oh"
+//    println(favoriteActor.length)
+//}
+
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//    println(favoriteActor.length)
+//}
+
+//fun main() {
+//    var favoriteActor: String? = null
+//    println(favoriteActor?.length)
+//}
+
+//fun main() {
+//    var favoriteActor: String? = null
+//    println(favoriteActor!!.length)
+//}
+//
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//
+//    if (favoriteActor != null) {
+//        println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+//    }
+//}
